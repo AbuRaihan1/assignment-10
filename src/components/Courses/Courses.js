1d839ceeacc4cdff9a9d9451402e2c353f9fb966
Abu Raihan <raihanahmed01973@gmail.com>
2023-03-11 22:34:59 +0600

    demo data fetch from server

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Courses.css";
 const Courses = () => {
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div>
       <h2>Courses</h2>
